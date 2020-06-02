65b414282fa2459ed3c4969a032e5a5bbb49ec05
lee123ho <lee123ho@naver.com>
2020-06-02 20:08:29 +0900

    Initial Commit

---
+++ SGPTermProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGPTermProject
+//
+//  Created by KPU_GAME on 2020/06/02.
+//  Copyright © 2020 KPU_GAME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
